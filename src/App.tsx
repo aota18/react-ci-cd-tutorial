a8b28889b4c6d03a5ab68a807bc1eda645adfdac
aota18 <sapphire031794@gmail.com>
2021-04-07 11:04:19 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Wow! It works!!
+        OMG!
         </p>
         <a
           className="App-link"
